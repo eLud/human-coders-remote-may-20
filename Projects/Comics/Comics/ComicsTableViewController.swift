4fb7a3562f02a71ee9b9f550d78e3757f756dad6
Ludovic Ollagnier <ludovic@tectec.fr>
2020-05-28 10:18:45 +0200

    UITableView Demo

--- Projects/Comics/Comics/ComicsTableViewController.swift
+++ Projects/Comics/Comics/ComicsTableViewController.swift
@@ -10,37 +10,43 @@ import UIKit
 
 class ComicsTableViewController: UITableViewController {
 
+    let library = Library()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        print("TableViewCOntroller loaded")
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.dataSource = self
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+
+        switch section {
+        case 0:
+            return library.allComics.count
+        default:
+            return 0
+        }
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "subtitleCell", for: indexPath)
+
+        let currentComic = library.allComics[indexPath.row]
+
+        cell.textLabel?.text = currentComic.title
+        cell.detailTextLabel?.text = currentComic.author
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -87,4 +93,17 @@ class ComicsTableViewController: UITableViewController {
     }
     */
 
+}
+
+extension ComicsTableViewController: UITableViewDataSourcePrefetching {
+
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+
+    }
+
+}
+
+extension ComicsTableViewController {
+
+//    var value: Int = 0 // Pas de stored properties
 }
